4a7ac236de5f79045058bd78440df21713a436ac
Tariq Yousuf <tariqryanyousuf@gmail.com>
2020-11-25 01:49:14 -0800

    Added 8 more Multipliers and used FixedPointAdders

--- Verilog/Neuron.v
+++ Verilog/Neuron.v
@@ -7,16 +7,16 @@ module Neuron (
 	input clk,
 	input GlobalReset,
 	input Input_valid,
-	input [18:0]  Wgt_0, Wgt_1, Wgt_2, Wgt_3, Wgt_4, Wgt_5, Wgt_6, Wgt_7, Wgt_8, Wgt_9, Wgt_10, Wgt_11, Wgt_12, Wgt_13, Wgt_14, Wgt_15, Wgt_16, Wgt_17, Wgt_18, Wgt_19, Wgt_20, Wgt_21, Wgt_22, Wgt_23, Wgt_24, Wgt_25, Wgt_26, Wgt_27, Wgt_28, Wgt_29, Wgt_30, Wgt_31,
-	input [9:0]  Pix_0, Pix_1, Pix_2, Pix_3, Pix_4, Pix_5, Pix_6, Pix_7, Pix_8, Pix_9, Pix_10, Pix_11, Pix_12, Pix_13, Pix_14, Pix_15, Pix_16, Pix_17, Pix_18, Pix_19, Pix_20, Pix_21, Pix_22, Pix_23, Pix_24, Pix_25, Pix_26, Pix_27, Pix_28, Pix_29, Pix_30, Pix_31,
+	input [18:0]  Wgt_0, Wgt_1, Wgt_2, Wgt_3, Wgt_4, Wgt_5, Wgt_6, Wgt_7, Wgt_8, Wgt_9, Wgt_10, Wgt_11, Wgt_12, Wgt_13, Wgt_14, Wgt_15, Wgt_16, Wgt_17, Wgt_18, Wgt_19, Wgt_20, Wgt_21, Wgt_22, Wgt_23, Wgt_24, Wgt_25, Wgt_26, Wgt_27, Wgt_28, Wgt_29, Wgt_30, Wgt_31, Wgt_32, Wgt_33, Wgt_34, Wgt_35, Wgt_36, Wgt_37, Wgt_38, Wgt_39, Wgt_40, Wgt_41, Wgt_42, Wgt_43, Wgt_44, Wgt_45, Wgt_46, Wgt_47, Wgt_48, Wgt_49, Wgt_50, Wgt_51, Wgt_52, Wgt_53, Wgt_54, Wgt_55, Wgt_56, Wgt_57, Wgt_58, Wgt_59, Wgt_60, Wgt_61, Wgt_62, Wgt_63,
+	input [9:0]  Pix_0, Pix_1, Pix_2, Pix_3, Pix_4, Pix_5, Pix_6, Pix_7, Pix_8, Pix_9, Pix_10, Pix_11, Pix_12, Pix_13, Pix_14, Pix_15, Pix_16, Pix_17, Pix_18, Pix_19, Pix_20, Pix_21, Pix_22, Pix_23, Pix_24, Pix_25, Pix_26, Pix_27, Pix_28, Pix_29, Pix_30, Pix_31, Pix_32, Pix_33, Pix_34, Pix_35, Pix_36, Pix_37, Pix_38, Pix_39, Pix_40, Pix_41, Pix_42, Pix_43, Pix_44, Pix_45, Pix_46, Pix_47, Pix_48, Pix_49, Pix_50, Pix_51, Pix_52, Pix_53, Pix_54, Pix_55, Pix_56, Pix_57, Pix_58, Pix_59, Pix_60, Pix_61, Pix_62, Pix_63,
 	output [25:0] Out,
 	output Output_valid
 );
 
 // FixedPointMultiplier I/0
-reg [18:0] M0_wgt, M1_wgt, M2_wgt, M3_wgt, M4_wgt, M5_wgt, M6_wgt, M7_wgt;   
-reg [9:0] M0_pix, M1_pix, M2_pix, M3_pix, M4_pix, M5_pix, M6_pix, M7_pix;   
-wire [25:0] M0, M1, M2, M3, M4, M5, M6, M7; 
+reg [18:0] M0_wgt, M1_wgt, M2_wgt, M3_wgt, M4_wgt, M5_wgt, M6_wgt, M7_wgt, M8_wgt, M9_wgt, M10_wgt, M11_wgt, M12_wgt, M13_wgt, M14_wgt, M15_wgt;   
+reg [9:0] M0_pix, M1_pix, M2_pix, M3_pix, M4_pix, M5_pix, M6_pix, M7_pix, M8_pix, M9_pix, M10_pix, M11_pix, M12_pix, M13_pix, M14_pix, M15_pix;   
+wire [25:0] M0, M1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M15; 
 
 // For Timing
 wire last_input;
@@ -36,19 +36,28 @@ FixedPointMultiplier Mult_4(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M4
 FixedPointMultiplier Mult_5(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M5_wgt), .PixelPort(M5_pix), .Output_syn(M5));
 FixedPointMultiplier Mult_6(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M6_wgt), .PixelPort(M6_pix), .Output_syn(M6));
 FixedPointMultiplier Mult_7(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M7_wgt), .PixelPort(M7_pix), .Output_syn(M7));
+FixedPointMultiplier Mult_8(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M8_wgt), .PixelPort(M8_pix), .Output_syn(M8));
+FixedPointMultiplier Mult_9(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M9_wgt), .PixelPort(M9_pix), .Output_syn(M9));
+FixedPointMultiplier Mult_10(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M10_wgt), .PixelPort(M10_pix), .Output_syn(M10));
+FixedPointMultiplier Mult_11(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M11_wgt), .PixelPort(M11_pix), .Output_syn(M11));
+FixedPointMultiplier Mult_12(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M12_wgt), .PixelPort(M12_pix), .Output_syn(M12));
+FixedPointMultiplier Mult_13(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M13_wgt), .PixelPort(M13_pix), .Output_syn(M13));
+FixedPointMultiplier Mult_14(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M14_wgt), .PixelPort(M14_pix), .Output_syn(M14));
+FixedPointMultiplier Mult_15(.clk(clk), .GlobalReset(GlobalReset), .WeightPort(M15_wgt), .PixelPort(M15_pix), .Output_syn(M15));
 
 
 
 // Counting Clock Cycles
-wire [2:0] mod_7_cntr; 
-mod_N_counter #(7, 3) m0(.clk(clk | Inc_output_valid), .GlobalReset(GlobalReset | Input_valid), .out(mod_7_cntr));
-wire next_set = (mod_7_cntr == 3'b110);    
+wire [3:0] mod_15_cntr; 
+mod_N_counter #(15, 4) m0(.clk(clk | Inc_output_valid), .GlobalReset(GlobalReset | Input_valid), .out(mod_15_cntr));
+wire next_set = (mod_15_cntr == 4'b1110);    
+//wire next_set = (mod_7_cntr == 3'b110);    
 wire [1:0] mod_4_counter;
 mod_N_counter #(4, 2) m1(.clk(next_set | last_input), .GlobalReset(GlobalReset | Input_valid), .out(mod_4_counter));
 
 // Turning off Multipliers/Adders when finished
 assign last_input = (mod_4_counter == 2'b11);
-assign Inc_output_valid = (mod_4_counter == 2'b11) & (mod_7_cntr == 3'b101);
+assign Inc_output_valid = (mod_4_counter == 2'b11) & (mod_15_cntr == 4'b1101);
 
 // Timing the inputs 
 always @ (*) begin
@@ -61,6 +70,14 @@ always @ (*) begin
 		M5_wgt = Wgt_5;
 		M6_wgt = Wgt_6;
 		M7_wgt = Wgt_7;
+		M8_wgt = Wgt_8;
+		M9_wgt = Wgt_9;
+		M10_wgt = Wgt_10;
+		M11_wgt = Wgt_11;
+		M12_wgt = Wgt_12;
+		M13_wgt = Wgt_13;
+		M14_wgt = Wgt_14;
+		M15_wgt = Wgt_15;
 		M0_pix = Pix_0;
 		M1_pix = Pix_1;
 		M2_pix = Pix_2;
@@ -69,26 +86,16 @@ always @ (*) begin
 		M5_pix = Pix_5;
 		M6_pix = Pix_6;
 		M7_pix = Pix_7;
+		M8_pix = Pix_8;
+		M9_pix = Pix_9;
+		M10_pix = Pix_10;
+		M11_pix = Pix_11;
+		M12_pix = Pix_12;
+		M13_pix = Pix_13;
+		M14_pix = Pix_14;
+		M15_pix = Pix_15;
 	end 
 	else if(mod_4_counter  == 2'b01) begin
-		M0_wgt = Wgt_8;
-		M1_wgt = Wgt_9;
-		M2_wgt = Wgt_10;
-		M3_wgt = Wgt_11;
-		M4_wgt = Wgt_12;
-		M5_wgt = Wgt_13;
-		M6_wgt = Wgt_14;
-		M7_wgt = Wgt_15;
-		M0_pix = Pix_8;
-		M1_pix = Pix_9;
-		M2_pix = Pix_10;
-		M3_pix = Pix_11;
-		M4_pix = Pix_12;
-		M5_pix = Pix_13;
-		M6_pix = Pix_14;
-		M7_pix = Pix_15;
-	end
-	else if(mod_4_counter  == 2'b10) begin
 		M0_wgt = Wgt_16;
 		M1_wgt = Wgt_17;
 		M2_wgt = Wgt_18;
@@ -97,6 +104,14 @@ always @ (*) begin
 		M5_wgt = Wgt_21;
 		M6_wgt = Wgt_22;
 		M7_wgt = Wgt_23;
+		M8_wgt = Wgt_24;
+		M9_wgt = Wgt_25;
+		M10_wgt = Wgt_26;
+		M11_wgt = Wgt_27;
+		M12_wgt = Wgt_28;
+		M13_wgt = Wgt_29;
+		M14_wgt = Wgt_30;
+		M15_wgt = Wgt_31;
 		M0_pix = Pix_16;
 		M1_pix = Pix_17;
 		M2_pix = Pix_18;
@@ -105,29 +120,110 @@ always @ (*) begin
 		M5_pix = Pix_21;
 		M6_pix = Pix_22;
 		M7_pix = Pix_23;
+		M8_pix = Pix_24;
+		M9_pix = Pix_25;
+		M10_pix = Pix_26;
+		M11_pix = Pix_27;
+		M12_pix = Pix_28;
+		M13_pix = Pix_29;
+		M14_pix = Pix_30;
+		M15_pix = Pix_31;
+	end
+	else if(mod_4_counter  == 2'b10) begin
+		M0_wgt = Wgt_32;
+		M1_wgt = Wgt_33;
+		M2_wgt = Wgt_34;
+		M3_wgt = Wgt_35;
+		M4_wgt = Wgt_36;
+		M5_wgt = Wgt_37;
+		M6_wgt = Wgt_38;
+		M7_wgt = Wgt_39;
+		M8_wgt = Wgt_40;
+		M9_wgt = Wgt_41;
+		M10_wgt = Wgt_42;
+		M11_wgt = Wgt_43;
+		M12_wgt = Wgt_44;
+		M13_wgt = Wgt_45;
+		M14_wgt = Wgt_46;
+		M15_wgt = Wgt_47;
+		M0_pix = Pix_32;
+		M1_pix = Pix_33;
+		M2_pix = Pix_34;
+		M3_pix = Pix_35;
+		M4_pix = Pix_36;
+		M5_pix = Pix_37;
+		M6_pix = Pix_38;
+		M7_pix = Pix_39;
+		M8_pix = Pix_40;
+		M9_pix = Pix_41;
+		M10_pix = Pix_42;
+		M11_pix = Pix_43;
+		M12_pix = Pix_44;
+		M13_pix = Pix_45;
+		M14_pix = Pix_46;
+		M15_pix = Pix_47;
 	end
 	else begin
-		M0_wgt = Wgt_24;
-		M1_wgt = Wgt_25;
-		M2_wgt = Wgt_26;
-		M3_wgt = Wgt_27;
-		M4_wgt = Wgt_28;
-		M5_wgt = Wgt_29;
-		M6_wgt = Wgt_30;
-		M7_wgt = Wgt_31;
-		M0_pix = Pix_24;
-		M1_pix = Pix_25;
-		M2_pix = Pix_26;
-		M3_pix = Pix_27;
-		M4_pix = Pix_28;
-		M5_pix = Pix_29;
-		M6_pix = Pix_30;
-		M7_pix = Pix_31;
+		M0_wgt = Wgt_48;
+		M1_wgt = Wgt_49;
+		M2_wgt = Wgt_50;
+		M3_wgt = Wgt_51;
+		M4_wgt = Wgt_52;
+		M5_wgt = Wgt_53;
+		M6_wgt = Wgt_54;
+		M7_wgt = Wgt_55;
+		M8_wgt = Wgt_56;
+		M9_wgt = Wgt_57;
+		M10_wgt = Wgt_58;
+		M11_wgt = Wgt_59;
+		M12_wgt = Wgt_60;
+		M13_wgt = Wgt_61;
+		M14_wgt = Wgt_62;
+		M15_wgt = Wgt_63;
+		M0_pix = Pix_48;
+		M1_pix = Pix_49;
+		M2_pix = Pix_50;
+		M3_pix = Pix_51;
+		M4_pix = Pix_52;
+		M5_pix = Pix_53;
+		M6_pix = Pix_54;
+		M7_pix = Pix_55;
+		M8_pix = Pix_56;
+		M9_pix = Pix_57;
+		M10_pix = Pix_58;
+		M11_pix = Pix_59;
+		M12_pix = Pix_60;
+		M13_pix = Pix_61;
+		M14_pix = Pix_62;
+		M15_pix = Pix_63;
 	end
 end
 
 
-assign adder_out = ((M0 + M1) + (M2 + M3)) + ((M4 + M5) + (M6 + M7));
+wire [25:0] Add_0_out, Add_1_out, Add_2_out, Add_3_out, Add_4_out, Add_5_out, Add_6_out, Add_7_out;
+FixedPointAdder Add_0(.clk(clk), .GlobalReset(GlobalReset), .Port2(M1), .Port1(M0), .Output_syn(Add_0_out));
+FixedPointAdder Add_1(.clk(clk), .GlobalReset(GlobalReset), .Port2(M3), .Port1(M2), .Output_syn(Add_1_out));
+FixedPointAdder Add_2(.clk(clk), .GlobalReset(GlobalReset), .Port2(M5), .Port1(M4), .Output_syn(Add_2_out));
+FixedPointAdder Add_3(.clk(clk), .GlobalReset(GlobalReset), .Port2(M7), .Port1(M6), .Output_syn(Add_3_out));
+FixedPointAdder Add_4(.clk(clk), .GlobalReset(GlobalReset), .Port2(M9), .Port1(M8), .Output_syn(Add_4_out));
+FixedPointAdder Add_5(.clk(clk), .GlobalReset(GlobalReset), .Port2(M11), .Port1(M10), .Output_syn(Add_5_out));
+FixedPointAdder Add_6(.clk(clk), .GlobalReset(GlobalReset), .Port2(M13), .Port1(M12), .Output_syn(Add_6_out));
+FixedPointAdder Add_7(.clk(clk), .GlobalReset(GlobalReset), .Port2(M15), .Port1(M14), .Output_syn(Add_7_out));
+
+wire [25:0] Add_8_out, Add_9_out, Add_10_out, Add_11_out;
+FixedPointAdder Add_8(.clk(clk), .GlobalReset(GlobalReset), .Port2(Add_1_out), .Port1(Add_0_out), .Output_syn(Add_8_out));
+FixedPointAdder Add_9(.clk(clk), .GlobalReset(GlobalReset), .Port2(Add_3_out), .Port1(Add_2_out), .Output_syn(Add_9_out));
+FixedPointAdder Add_10(.clk(clk), .GlobalReset(GlobalReset), .Port2(Add_5_out), .Port1(Add_4_out), .Output_syn(Add_10_out));
+FixedPointAdder Add_11(.clk(clk), .GlobalReset(GlobalReset), .Port2(Add_7_out), .Port1(Add_6_out), .Output_syn(Add_11_out));
+
+wire [25:0] Add_12_out, Add_13_out;
+FixedPointAdder Add_12(.clk(clk), .GlobalReset(GlobalReset), .Port2(Add_9_out), .Port1(Add_8_out), .Output_syn(Add_12_out));
+FixedPointAdder Add_13(.clk(clk), .GlobalReset(GlobalReset), .Port2(Add_11_out), .Port1(Add_10_out), .Output_syn(Add_13_out));
+
+FixedPointAdder Add_14(.clk(clk), .GlobalReset(GlobalReset), .Port2(Add_12_out), .Port1(Add_13_out), .Output_syn(adder_out));
+
+//assign adder_out = ((M0 + M1) + (M2 + M3)) + ((M4 + M5) + (M6 + M7)) + ((M8 + M9) + (M10 + M11)) + ((M12 + M13) + (M14 + M15));
+
 
 // Adding current set to previous sets
 wire sample;
--- Verilog/Neuron_tb.v
+++ Verilog/Neuron_tb.v
@@ -39,6 +39,38 @@ reg [18:0] Wgt_28 = 19'b0000000000000001001;
 reg [18:0] Wgt_29 = 19'b1111111111111110111;
 reg [18:0] Wgt_30 = 19'b0000000000000001001;
 reg [18:0] Wgt_31 = 19'b0000000000000110101;
+reg [18:0] Wgt_32 = 19'b0000000000000000001;
+reg [18:0] Wgt_33 = 19'b1111111111111111101;
+reg [18:0] Wgt_34 = 19'b1111111011111010000;
+reg [18:0] Wgt_35 = 19'b0000000000001101101;
+reg [18:0] Wgt_36 = 19'b0000000000000110011;
+reg [18:0] Wgt_37 = 19'b0000000000000110101;
+reg [18:0] Wgt_38 = 19'b0000000000000000000;
+reg [18:0] Wgt_39 = 19'b0000000000000000001;
+reg [18:0] Wgt_40 = 19'b0000000000000011100;
+reg [18:0] Wgt_41 = 19'b0000000000000101011;
+reg [18:0] Wgt_42 = 19'b1111111111111011010;
+reg [18:0] Wgt_43 = 19'b0000000000000000001;
+reg [18:0] Wgt_44 = 19'b0000000000000000000;
+reg [18:0] Wgt_45 = 19'b0000000000000001100;
+reg [18:0] Wgt_46 = 19'b0000000000000000000;
+reg [18:0] Wgt_47 = 19'b0000000000000000001;
+reg [18:0] Wgt_48 = 19'b0000000000000001001;
+reg [18:0] Wgt_49 = 19'b0000000000000110101;
+reg [18:0] Wgt_50 = 19'b1111111111111111000;
+reg [18:0] Wgt_51 = 19'b1111111111111110111;
+reg [18:0] Wgt_52 = 19'b0000000000000001001;
+reg [18:0] Wgt_53 = 19'b1111111111111110111;
+reg [18:0] Wgt_54 = 19'b0000000000000110101;
+reg [18:0] Wgt_55 = 19'b1111111111111110111;
+reg [18:0] Wgt_56 = 19'b1111111111111111000;
+reg [18:0] Wgt_57 = 19'b0000000000000001001;
+reg [18:0] Wgt_58 = 19'b0000000000000110101;
+reg [18:0] Wgt_59 = 19'b1111111111111110111;
+reg [18:0] Wgt_60 = 19'b0000000000000001001;
+reg [18:0] Wgt_61 = 19'b1111111111111110111;
+reg [18:0] Wgt_62 = 19'b0000000000000001001;
+reg [18:0] Wgt_63 = 19'b0000000000000110101;
 reg [9:0] Pix_0;
 reg [9:0] Pix_1;
 reg [9:0] Pix_2;
@@ -71,12 +103,45 @@ reg [9:0] Pix_28;
 reg [9:0] Pix_29;
 reg [9:0] Pix_30;
 reg [9:0] Pix_31;
+reg [9:0] Pix_32;
+reg [9:0] Pix_33;
+reg [9:0] Pix_34;
+reg [9:0] Pix_35;
+reg [9:0] Pix_36;
+reg [9:0] Pix_37;
+reg [9:0] Pix_38;
+reg [9:0] Pix_39;
+reg [9:0] Pix_40;
+reg [9:0] Pix_41;
+reg [9:0] Pix_42;
+reg [9:0] Pix_43;
+reg [9:0] Pix_44;
+reg [9:0] Pix_45;
+reg [9:0] Pix_46;
+reg [9:0] Pix_47;
+reg [9:0] Pix_48;
+reg [9:0] Pix_49;
+reg [9:0] Pix_50;
+reg [9:0] Pix_51;
+reg [9:0] Pix_52;
+reg [9:0] Pix_53;
+reg [9:0] Pix_54;
+reg [9:0] Pix_55;
+reg [9:0] Pix_56;
+reg [9:0] Pix_57;
+reg [9:0] Pix_58;
+reg [9:0] Pix_59;
+reg [9:0] Pix_60;
+reg [9:0] Pix_61;
+reg [9:0] Pix_62;
+reg [9:0] Pix_63;
 wire [25:0] Out;
 wire Output_valid;
 
 
 // Neuron Instantiation
-Neuron N_0(.clk(clk), .GlobalReset(GlobalReset), .Input_valid(Input_valid), .Wgt_0(Wgt_0), .Wgt_1(Wgt_1), .Wgt_2(Wgt_2), .Wgt_3(Wgt_3), .Wgt_4(Wgt_4), .Wgt_5(Wgt_5), .Wgt_6(Wgt_6), .Wgt_7(Wgt_7), .Wgt_8(Wgt_8), .Wgt_9(Wgt_9), .Wgt_10(Wgt_10), .Wgt_11(Wgt_11), .Wgt_12(Wgt_12), .Wgt_13(Wgt_13), .Wgt_14(Wgt_14), .Wgt_15(Wgt_15), .Wgt_16(Wgt_16), .Wgt_17(Wgt_17), .Wgt_18(Wgt_18), .Wgt_19(Wgt_19), .Wgt_20(Wgt_20), .Wgt_21(Wgt_21), .Wgt_22(Wgt_22), .Wgt_23(Wgt_23), .Wgt_24(Wgt_24), .Wgt_25(Wgt_25), .Wgt_26(Wgt_26), .Wgt_27(Wgt_27), .Wgt_28(Wgt_28), .Wgt_29(Wgt_29), .Wgt_30(Wgt_30), .Wgt_31(Wgt_31), .Pix_0(Pix_0), .Pix_1(Pix_1), .Pix_2(Pix_2), .Pix_3(Pix_3), .Pix_4(Pix_4), .Pix_5(Pix_5), .Pix_6(Pix_6), .Pix_7(Pix_7), .Pix_8(Pix_8), .Pix_9(Pix_9), .Pix_10(Pix_10), .Pix_11(Pix_11), .Pix_12(Pix_12), .Pix_13(Pix_13), .Pix_14(Pix_14), .Pix_15(Pix_15), .Pix_16(Pix_16), .Pix_17(Pix_17), .Pix_18(Pix_18), .Pix_19(Pix_19), .Pix_20(Pix_20), .Pix_21(Pix_21), .Pix_22(Pix_22), .Pix_23(Pix_23), .Pix_24(Pix_24), .Pix_25(Pix_25), .Pix_26(Pix_26), .Pix_27(Pix_27), .Pix_28(Pix_28), .Pix_29(Pix_29), .Pix_30(Pix_30), .Pix_31(Pix_31), .Out(Out), .Output_valid(Output_valid));
+Neuron N_0(.clk(clk), .GlobalReset(GlobalReset), .Input_valid(Input_valid), .Wgt_0(Wgt_0), .Wgt_1(Wgt_1), .Wgt_2(Wgt_2), .Wgt_3(Wgt_3), .Wgt_4(Wgt_4), .Wgt_5(Wgt_5), .Wgt_6(Wgt_6), .Wgt_7(Wgt_7), .Wgt_8(Wgt_8), .Wgt_9(Wgt_9), .Wgt_10(Wgt_10), .Wgt_11(Wgt_11), .Wgt_12(Wgt_12), .Wgt_13(Wgt_13), .Wgt_14(Wgt_14), .Wgt_15(Wgt_15), .Wgt_16(Wgt_16), .Wgt_17(Wgt_17), .Wgt_18(Wgt_18), .Wgt_19(Wgt_19), .Wgt_20(Wgt_20), .Wgt_21(Wgt_21), .Wgt_22(Wgt_22), .Wgt_23(Wgt_23), .Wgt_24(Wgt_24), .Wgt_25(Wgt_25), .Wgt_26(Wgt_26), .Wgt_27(Wgt_27), .Wgt_28(Wgt_28), .Wgt_29(Wgt_29), .Wgt_30(Wgt_30), .Wgt_31(Wgt_31), .Wgt_32(Wgt_32), .Wgt_33(Wgt_33), .Wgt_34(Wgt_34), .Wgt_35(Wgt_35), .Wgt_36(Wgt_36), .Wgt_37(Wgt_37), .Wgt_38(Wgt_38), .Wgt_39(Wgt_39), .Wgt_40(Wgt_40), .Wgt_41(Wgt_41), .Wgt_42(Wgt_42), .Wgt_43(Wgt_43), .Wgt_44(Wgt_44), .Wgt_45(Wgt_45), .Wgt_46(Wgt_46), .Wgt_47(Wgt_47), .Wgt_48(Wgt_48), .Wgt_49(Wgt_49), .Wgt_50(Wgt_50), .Wgt_51(Wgt_51), .Wgt_52(Wgt_52), .Wgt_53(Wgt_53), .Wgt_54(Wgt_54), .Wgt_55(Wgt_55), .Wgt_56(Wgt_56), .Wgt_57(Wgt_57), .Wgt_58(Wgt_58), .Wgt_59(Wgt_59), .Wgt_60(Wgt_60), .Wgt_61(Wgt_61), .Wgt_62(Wgt_62), .Wgt_63(Wgt_63), .Pix_0(Pix_0), .Pix_1(Pix_1), .Pix_2(Pix_2), .Pix_3(Pix_3), .Pix_4(Pix_4), .Pix_5(Pix_5), .Pix_6(Pix_6), .Pix_7(Pix_7), .Pix_8(Pix_8), .Pix_9(Pix_9), .Pix_10(Pix_10), .Pix_11(Pix_11), .Pix_12(Pix_12), .Pix_13(Pix_13), .Pix_14(Pix_14), .Pix_15(Pix_15), .Pix_16(Pix_16), .Pix_17(Pix_17), .Pix_18(Pix_18), .Pix_19(Pix_19), .Pix_20(Pix_20), .Pix_21(Pix_21), .Pix_22(Pix_22), .Pix_23(Pix_23), .Pix_24(Pix_24), .Pix_25(Pix_25), .Pix_26(Pix_26), .Pix_27(Pix_27), .Pix_28(Pix_28), .Pix_29(Pix_29), .Pix_30(Pix_30), .Pix_31(Pix_31), .Pix_32(Pix_32), .Pix_33(Pix_33), .Pix_34(Pix_34), .Pix_35(Pix_35), .Pix_36(Pix_36), .Pix_37(Pix_37), .Pix_38(Pix_38), .Pix_39(Pix_39), .Pix_40(Pix_40), .Pix_41(Pix_41), .Pix_42(Pix_42), .Pix_43(Pix_43), .Pix_44(Pix_44), .Pix_45(Pix_45), .Pix_46(Pix_46), .Pix_47(Pix_47), .Pix_48(Pix_48), .Pix_49(Pix_49), .Pix_50(Pix_50), .Pix_51(Pix_51), .Pix_52(Pix_52), .Pix_53(Pix_53), .Pix_54(Pix_54), .Pix_55(Pix_55), .Pix_56(Pix_56), .Pix_57(Pix_57), .Pix_58(Pix_58), .Pix_59(Pix_59), .Pix_60(Pix_60), .Pix_61(Pix_61), .Pix_62(Pix_62), .Pix_63(Pix_63), .Out(Out), .Output_valid(Output_valid));
+
 
 parameter halfclock=1;
 parameter fullclock=2*halfclock;
@@ -90,39 +155,39 @@ initial begin
 	Input_valid = 1'b0;
 	#halfclock;
 	#halfclock;
-	Pix_0 = 0; Pix_1 = 1; Pix_2 = 2; Pix_3 = 3; Pix_4 = 4; Pix_5 = 5; Pix_6 = 6; Pix_7 = 7; Pix_8 = 8; Pix_9 = 9; Pix_10 = 10; Pix_11 = 11; Pix_12 = 12; Pix_13 = 13; Pix_14 = 14; Pix_15 = 15; Pix_16 = 16; Pix_17 = 17; Pix_18 = 18; Pix_19 = 19; Pix_20 = 20; Pix_21 = 21; Pix_22 = 22; Pix_23 = 23; Pix_24 = 24; Pix_25 = 25; Pix_26 = 26; Pix_27 = 27; Pix_28 = 28; Pix_29 = 29; Pix_30 = 30; Pix_31 = 31; 
+	Pix_0 = 0; Pix_1 = 1; Pix_2 = 2; Pix_3 = 3; Pix_4 = 4; Pix_5 = 5; Pix_6 = 6; Pix_7 = 7; Pix_8 = 8; Pix_9 = 9; Pix_10 = 10; Pix_11 = 11; Pix_12 = 12; Pix_13 = 13; Pix_14 = 14; Pix_15 = 15; Pix_16 = 16; Pix_17 = 17; Pix_18 = 18; Pix_19 = 19; Pix_20 = 20; Pix_21 = 21; Pix_22 = 22; Pix_23 = 23; Pix_24 = 24; Pix_25 = 25; Pix_26 = 26; Pix_27 = 27; Pix_28 = 28; Pix_29 = 29; Pix_30 = 30; Pix_31 = 31; Pix_32 = 32; Pix_33 = 33; Pix_34 = 34; Pix_35 = 35; Pix_36 = 36; Pix_37 = 37; Pix_38 = 38; Pix_39 = 39; Pix_40 = 40; Pix_41 = 41; Pix_42 = 42; Pix_43 = 43; Pix_44 = 44; Pix_45 = 45; Pix_46 = 46; Pix_47 = 47; Pix_48 = 48; Pix_49 = 49; Pix_50 = 50; Pix_51 = 51; Pix_52 = 52; Pix_53 = 53; Pix_54 = 54; Pix_55 = 55; Pix_56 = 56; Pix_57 = 57; Pix_58 = 58; Pix_59 = 59; Pix_60 = 60; Pix_61 = 61; Pix_62 = 62; Pix_63 = 63; 
 	GlobalReset = 1'b0;
 	Input_valid = 1'b1;
    #(10*fullclock);
    Input_valid = 1'b0;
-   #(7*fullclock);
-   true_out = {{7{Wgt_0[17]}}, Wgt_0} * Pix_0 + {{7{Wgt_1[17]}}, Wgt_1} * Pix_1 + {{7{Wgt_2[17]}}, Wgt_2} * Pix_2 + {{7{Wgt_3[17]}}, Wgt_3} * Pix_3 + {{7{Wgt_4[17]}}, Wgt_4} * Pix_4 + {{7{Wgt_5[17]}}, Wgt_5} * Pix_5 + {{7{Wgt_6[17]}}, Wgt_6} * Pix_6 + {{7{Wgt_7[17]}}, Wgt_7} * Pix_7;
+   #(15*fullclock);
+   true_out = {{7{Wgt_0[17]}}, Wgt_0} * Pix_0 + {{7{Wgt_1[17]}}, Wgt_1} * Pix_1 + {{7{Wgt_2[17]}}, Wgt_2} * Pix_2 + {{7{Wgt_3[17]}}, Wgt_3} * Pix_3 + {{7{Wgt_4[17]}}, Wgt_4} * Pix_4 + {{7{Wgt_5[17]}}, Wgt_5} * Pix_5 + {{7{Wgt_6[17]}}, Wgt_6} * Pix_6 + {{7{Wgt_7[17]}}, Wgt_7} * Pix_7 + {{7{Wgt_8[17]}}, Wgt_8} * Pix_8 + {{7{Wgt_9[17]}}, Wgt_9} * Pix_9 + {{7{Wgt_10[17]}}, Wgt_10} * Pix_10 + {{7{Wgt_11[17]}}, Wgt_11} * Pix_11 + {{7{Wgt_12[17]}}, Wgt_12} * Pix_12 + {{7{Wgt_13[17]}}, Wgt_13} * Pix_13 + {{7{Wgt_14[17]}}, Wgt_14} * Pix_14 + {{7{Wgt_15[17]}}, Wgt_15} * Pix_15;
    if ( true_out == Out)
 		$display("Matched 1!!");
 	else
 	   $display("Out: %b, true_out: %b", Out, true_out);
-   #(7*fullclock);
-   true_out = true_out + {{7{Wgt_8[17]}}, Wgt_8} * Pix_8 + {{7{Wgt_9[17]}}, Wgt_9} * Pix_9 + {{7{Wgt_10[17]}}, Wgt_10} * Pix_10 + {{7{Wgt_11[17]}}, Wgt_11} * Pix_11 + {{7{Wgt_12[17]}}, Wgt_12} * Pix_12 + {{7{Wgt_13[17]}}, Wgt_13} * Pix_13 + {{7{Wgt_14[17]}}, Wgt_14} * Pix_14 + {{7{Wgt_15[17]}}, Wgt_15} * Pix_15;                                                                                                                                                                                                                                                                                                   
+   #(15*fullclock);
+   true_out = true_out + {{7{Wgt_16[17]}}, Wgt_16} * Pix_16 + {{7{Wgt_17[17]}}, Wgt_17} * Pix_17 + {{7{Wgt_18[17]}}, Wgt_18} * Pix_18 + {{7{Wgt_19[17]}}, Wgt_19} * Pix_19 + {{7{Wgt_20[17]}}, Wgt_20} * Pix_20 + {{7{Wgt_21[17]}}, Wgt_21} * Pix_21 + {{7{Wgt_22[17]}}, Wgt_22} * Pix_22 + {{7{Wgt_23[17]}}, Wgt_23} * Pix_23 + {{7{Wgt_24[17]}}, Wgt_24} * Pix_24 + {{7{Wgt_25[17]}}, Wgt_25} * Pix_25 + {{7{Wgt_26[17]}}, Wgt_26} * Pix_26 + {{7{Wgt_27[17]}}, Wgt_27} * Pix_27 + {{7{Wgt_28[17]}}, Wgt_28} * Pix_28 + {{7{Wgt_29[17]}}, Wgt_29} * Pix_29 + {{7{Wgt_30[17]}}, Wgt_30} * Pix_30 + {{7{Wgt_31[17]}}, Wgt_31} * Pix_31;
 	if ( true_out == Out) begin
 		$display("Matched 2!!");
 	end
 	else
       $display("Out: %b, true_out: %b", Out, true_out);
-	#(7*fullclock);
-   true_out = true_out + {{7{Wgt_16[17]}}, Wgt_16} * Pix_16 + {{7{Wgt_17[17]}}, Wgt_17} * Pix_17 + {{7{Wgt_18[17]}}, Wgt_18} * Pix_18 + {{7{Wgt_19[17]}}, Wgt_19} * Pix_19 + {{7{Wgt_20[17]}}, Wgt_20} * Pix_20 + {{7{Wgt_21[17]}}, Wgt_21} * Pix_21 + {{7{Wgt_22[17]}}, Wgt_22} * Pix_22 + {{7{Wgt_23[17]}}, Wgt_23} * Pix_23;	
-   if ( true_out == Out) begin
+	#(15*fullclock);
+    true_out = true_out + {{7{Wgt_32[17]}}, Wgt_32} * Pix_32 + {{7{Wgt_33[17]}}, Wgt_33} * Pix_33 + {{7{Wgt_34[17]}}, Wgt_34} * Pix_34 + {{7{Wgt_35[17]}}, Wgt_35} * Pix_35 + {{7{Wgt_36[17]}}, Wgt_36} * Pix_36 + {{7{Wgt_37[17]}}, Wgt_37} * Pix_37 + {{7{Wgt_38[17]}}, Wgt_38} * Pix_38 + {{7{Wgt_39[17]}}, Wgt_39} * Pix_39 + {{7{Wgt_40[17]}}, Wgt_40} * Pix_40 + {{7{Wgt_41[17]}}, Wgt_41} * Pix_41 + {{7{Wgt_42[17]}}, Wgt_42} * Pix_42 + {{7{Wgt_43[17]}}, Wgt_43} * Pix_43 + {{7{Wgt_44[17]}}, Wgt_44} * Pix_44 + {{7{Wgt_45[17]}}, Wgt_45} * Pix_45 + {{7{Wgt_46[17]}}, Wgt_46} * Pix_46 + {{7{Wgt_47[17]}}, Wgt_47} * Pix_47;
+	if ( true_out == Out) begin
 		$display("Matched 3!!");
 	end
 	else
       $display("Out: %b, true_out: %b", Out, true_out);
-	#(7*fullclock);
-   true_out = true_out + {{7{Wgt_24[17]}}, Wgt_24} * Pix_24 + {{7{Wgt_25[17]}}, Wgt_25} * Pix_25 + {{7{Wgt_26[17]}}, Wgt_26} * Pix_26 + {{7{Wgt_27[17]}}, Wgt_27} * Pix_27 + {{7{Wgt_28[17]}}, Wgt_28} * Pix_28 + {{7{Wgt_29[17]}}, Wgt_29} * Pix_29 + {{7{Wgt_30[17]}}, Wgt_30} * Pix_30 + {{7{Wgt_31[17]}}, Wgt_31} * Pix_31;
+	#(15*fullclock);
+   true_out = true_out + {{7{Wgt_48[17]}}, Wgt_48} * Pix_48 + {{7{Wgt_49[17]}}, Wgt_49} * Pix_49 + {{7{Wgt_50[17]}}, Wgt_50} * Pix_50 + {{7{Wgt_51[17]}}, Wgt_51} * Pix_51 + {{7{Wgt_52[17]}}, Wgt_52} * Pix_52 + {{7{Wgt_53[17]}}, Wgt_53} * Pix_53 + {{7{Wgt_54[17]}}, Wgt_54} * Pix_54 + {{7{Wgt_55[17]}}, Wgt_55} * Pix_55 + {{7{Wgt_56[17]}}, Wgt_56} * Pix_56 + {{7{Wgt_57[17]}}, Wgt_57} * Pix_57 + {{7{Wgt_58[17]}}, Wgt_58} * Pix_58 + {{7{Wgt_59[17]}}, Wgt_59} * Pix_59 + {{7{Wgt_60[17]}}, Wgt_60} * Pix_60 + {{7{Wgt_61[17]}}, Wgt_61} * Pix_61 + {{7{Wgt_62[17]}}, Wgt_62} * Pix_62 + {{7{Wgt_63[17]}}, Wgt_63} * Pix_63;
    if ( true_out == Out) begin
 		$display("Matched 4!!");
 	end
 	else
       $display("Out: %b, true_out: %b", Out, true_out);
-   #(10*fullclock);
+   #(15*fullclock);
 	if ( (true_out == Out) & (Output_valid == 1)) begin
 	   $display("Matched End!!");
    end
@@ -137,40 +202,39 @@ initial begin
    Input_valid = 1'b1;
    #(10*fullclock);
    Input_valid = 1'b0;
-   #(7*fullclock);
-   true_out = {{7{Wgt_0[17]}}, Wgt_0} * Pix_0 + {{7{Wgt_1[17]}}, Wgt_1} * Pix_1 + {{7{Wgt_2[17]}}, Wgt_2} * Pix_2 + {{7{Wgt_3[17]}}, Wgt_3} * Pix_3 + {{7{Wgt_4[17]}}, Wgt_4} * Pix_4 + {{7{Wgt_5[17]}}, Wgt_5} * Pix_5 + {{7{Wgt_6[17]}}, Wgt_6} * Pix_6 + {{7{Wgt_7[17]}}, Wgt_7} * Pix_7;
+   #(15*fullclock);
+   true_out = {{7{Wgt_0[17]}}, Wgt_0} * Pix_0 + {{7{Wgt_1[17]}}, Wgt_1} * Pix_1 + {{7{Wgt_2[17]}}, Wgt_2} * Pix_2 + {{7{Wgt_3[17]}}, Wgt_3} * Pix_3 + {{7{Wgt_4[17]}}, Wgt_4} * Pix_4 + {{7{Wgt_5[17]}}, Wgt_5} * Pix_5 + {{7{Wgt_6[17]}}, Wgt_6} * Pix_6 + {{7{Wgt_7[17]}}, Wgt_7} * Pix_7 + {{7{Wgt_8[17]}}, Wgt_8} * Pix_8 + {{7{Wgt_9[17]}}, Wgt_9} * Pix_9 + {{7{Wgt_10[17]}}, Wgt_10} * Pix_10 + {{7{Wgt_11[17]}}, Wgt_11} * Pix_11 + {{7{Wgt_12[17]}}, Wgt_12} * Pix_12 + {{7{Wgt_13[17]}}, Wgt_13} * Pix_13 + {{7{Wgt_14[17]}}, Wgt_14} * Pix_14 + {{7{Wgt_15[17]}}, Wgt_15} * Pix_15;
    if ( true_out == Out)
 		$display("Matched 1!!");
 	else
 	   $display("Out: %b, true_out: %b", Out, true_out);
-   #(7*fullclock);
-   true_out = true_out + {{7{Wgt_8[17]}}, Wgt_8} * Pix_8 + {{7{Wgt_9[17]}}, Wgt_9} * Pix_9 + {{7{Wgt_10[17]}}, Wgt_10} * Pix_10 + {{7{Wgt_11[17]}}, Wgt_11} * Pix_11 + {{7{Wgt_12[17]}}, Wgt_12} * Pix_12 + {{7{Wgt_13[17]}}, Wgt_13} * Pix_13 + {{7{Wgt_14[17]}}, Wgt_14} * Pix_14 + {{7{Wgt_15[17]}}, Wgt_15} * Pix_15;                                                                                                                                                                                                                                                                                                   
+   #(15*fullclock);
+   true_out = true_out + {{7{Wgt_16[17]}}, Wgt_16} * Pix_16 + {{7{Wgt_17[17]}}, Wgt_17} * Pix_17 + {{7{Wgt_18[17]}}, Wgt_18} * Pix_18 + {{7{Wgt_19[17]}}, Wgt_19} * Pix_19 + {{7{Wgt_20[17]}}, Wgt_20} * Pix_20 + {{7{Wgt_21[17]}}, Wgt_21} * Pix_21 + {{7{Wgt_22[17]}}, Wgt_22} * Pix_22 + {{7{Wgt_23[17]}}, Wgt_23} * Pix_23 + {{7{Wgt_24[17]}}, Wgt_24} * Pix_24 + {{7{Wgt_25[17]}}, Wgt_25} * Pix_25 + {{7{Wgt_26[17]}}, Wgt_26} * Pix_26 + {{7{Wgt_27[17]}}, Wgt_27} * Pix_27 + {{7{Wgt_28[17]}}, Wgt_28} * Pix_28 + {{7{Wgt_29[17]}}, Wgt_29} * Pix_29 + {{7{Wgt_30[17]}}, Wgt_30} * Pix_30 + {{7{Wgt_31[17]}}, Wgt_31} * Pix_31;
 	if ( true_out == Out) begin
 		$display("Matched 2!!");
 	end
 	else
       $display("Out: %b, true_out: %b", Out, true_out);
-	#(7*fullclock);
-   true_out = true_out + {{7{Wgt_16[17]}}, Wgt_16} * Pix_16 + {{7{Wgt_17[17]}}, Wgt_17} * Pix_17 + {{7{Wgt_18[17]}}, Wgt_18} * Pix_18 + {{7{Wgt_19[17]}}, Wgt_19} * Pix_19 + {{7{Wgt_20[17]}}, Wgt_20} * Pix_20 + {{7{Wgt_21[17]}}, Wgt_21} * Pix_21 + {{7{Wgt_22[17]}}, Wgt_22} * Pix_22 + {{7{Wgt_23[17]}}, Wgt_23} * Pix_23;	
-   if ( true_out == Out) begin
+	#(15*fullclock);
+    true_out = true_out + {{7{Wgt_32[17]}}, Wgt_32} * Pix_32 + {{7{Wgt_33[17]}}, Wgt_33} * Pix_33 + {{7{Wgt_34[17]}}, Wgt_34} * Pix_34 + {{7{Wgt_35[17]}}, Wgt_35} * Pix_35 + {{7{Wgt_36[17]}}, Wgt_36} * Pix_36 + {{7{Wgt_37[17]}}, Wgt_37} * Pix_37 + {{7{Wgt_38[17]}}, Wgt_38} * Pix_38 + {{7{Wgt_39[17]}}, Wgt_39} * Pix_39 + {{7{Wgt_40[17]}}, Wgt_40} * Pix_40 + {{7{Wgt_41[17]}}, Wgt_41} * Pix_41 + {{7{Wgt_42[17]}}, Wgt_42} * Pix_42 + {{7{Wgt_43[17]}}, Wgt_43} * Pix_43 + {{7{Wgt_44[17]}}, Wgt_44} * Pix_44 + {{7{Wgt_45[17]}}, Wgt_45} * Pix_45 + {{7{Wgt_46[17]}}, Wgt_46} * Pix_46 + {{7{Wgt_47[17]}}, Wgt_47} * Pix_47;
+	if ( true_out == Out) begin
 		$display("Matched 3!!");
 	end
 	else
       $display("Out: %b, true_out: %b", Out, true_out);
-	#(7*fullclock);
-   true_out = true_out + {{7{Wgt_24[17]}}, Wgt_24} * Pix_24 + {{7{Wgt_25[17]}}, Wgt_25} * Pix_25 + {{7{Wgt_26[17]}}, Wgt_26} * Pix_26 + {{7{Wgt_27[17]}}, Wgt_27} * Pix_27 + {{7{Wgt_28[17]}}, Wgt_28} * Pix_28 + {{7{Wgt_29[17]}}, Wgt_29} * Pix_29 + {{7{Wgt_30[17]}}, Wgt_30} * Pix_30 + {{7{Wgt_31[17]}}, Wgt_31} * Pix_31;
+	#(15*fullclock);
+   true_out = true_out + {{7{Wgt_48[17]}}, Wgt_48} * Pix_48 + {{7{Wgt_49[17]}}, Wgt_49} * Pix_49 + {{7{Wgt_50[17]}}, Wgt_50} * Pix_50 + {{7{Wgt_51[17]}}, Wgt_51} * Pix_51 + {{7{Wgt_52[17]}}, Wgt_52} * Pix_52 + {{7{Wgt_53[17]}}, Wgt_53} * Pix_53 + {{7{Wgt_54[17]}}, Wgt_54} * Pix_54 + {{7{Wgt_55[17]}}, Wgt_55} * Pix_55 + {{7{Wgt_56[17]}}, Wgt_56} * Pix_56 + {{7{Wgt_57[17]}}, Wgt_57} * Pix_57 + {{7{Wgt_58[17]}}, Wgt_58} * Pix_58 + {{7{Wgt_59[17]}}, Wgt_59} * Pix_59 + {{7{Wgt_60[17]}}, Wgt_60} * Pix_60 + {{7{Wgt_61[17]}}, Wgt_61} * Pix_61 + {{7{Wgt_62[17]}}, Wgt_62} * Pix_62 + {{7{Wgt_63[17]}}, Wgt_63} * Pix_63;
    if ( true_out == Out) begin
 		$display("Matched 4!!");
 	end
 	else
       $display("Out: %b, true_out: %b", Out, true_out);
-   #(10*fullclock);
-	if ((true_out == Out) & (Output_valid == 1)) begin
+   #(15*fullclock);
+	if ( (true_out == Out) & (Output_valid == 1)) begin
 	   $display("Matched End!!");
    end
    else
       $display("Out: %b, true_out: %b", Out, true_out);
-	
 	
 	
 	
